a24596e757d61a0df55635d73558e3c45cb9474a
minhajshafiq <minhaj93500@gmail.com>
2024-12-14 11:45:17 +0100

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix: process.env.ASSET_PREFIX,
-    basePath: process.env.BASE_PATH,
+    experimental: {
+        appDir: true,
+    },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
